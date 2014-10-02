afd24930677e94f3868384c615e650d6fa940450
Ion Caliman <starsoftcaliman@gmail.com>
2014-10-02 17:55:09 +0300

    added metadata with dependencies

--- deploy/recipes/web2py.rb
+++ deploy/recipes/web2py.rb
@@ -1,5 +1,6 @@
 include_recipe 'deploy'
 include_recipe 'nginx::service'
+include_recipe 'uwsgi::service'
 
 node[:opsworks][:applications].each do |app|
   application = app[:slug_name]
--- web2py/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-
-default[:web2py][:default_application] = nil
